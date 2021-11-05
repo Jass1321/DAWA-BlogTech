02588c02b27ff70287084a24fd0c5b8e1b7d3f2b
Jass1321 <jassyra13216@gmail.com>
2021-11-05 16:07:22 -0500

    ft add user model

--- store/dummy.js
+++ store/dummy.js
@@ -1,59 +1,19 @@
-//local storage
-const db = {
-    users: [
-        {
-            //debe ser texto para que no compare con un numero
-            id:"USER01",
-            name: "Jassyra",
-            last_name: "Espinoza",
-            email: "jass1321@gmail.com",
-            password: "J4SS",
-        },
-        {
-            id: "USER02",
-            name: "Kiara",
-            last_name: "Pinedo",
-            email: "kiara5678@gmail.com",
-            password: "K14R4",
-        },
-    ],
-    stories: [
-        {
-            id: "H100",
-            title: "Historia 1",
-            author: "Jaime Gómez",
-            text: "Programar es un arte",
-            dataTime: "2012-04-23",
-            user_id: "USER01",
-        },
-        {
-            id: "H200",
-            title: "Historia 2",
-            author: "Linder Hassinger",
-            text: "Okei chicos chicas xd",
-            dataTime: "2021-10-23",
-            user_id: "USER02",
-        },
-    ],
-    comments: [
-        {
-            id: "lana",
-            comment: "leviosa",
-            author: "kkaka",
-        }
-    ]
-};
-
-//Aqui vamos a poner las ope basicas - CRUD
 //*todo esto debe ser con asynce await
 export const list = async (table) => {
     return await db[table];
 };
 
-export const store = async (table, data) => {
+/**
+ * 
+ * @param {any} model 
+ * @param {Array<any>} data 
+ * @returns 
+ */
+
+export const store = async (model, data) => {
     //*Creo un dato y retorno la lista completa
-    await db[table].push(data);
-    return await list(table);
+    const object = new model(data);
+    object.save();
 };
 
 export const find = async (table, id) => {
